abf29019b1c8fdcfe9a4bf96faa4e687f85b10b1
daayaan3288 <dayannn@gmail.com>
2023-10-09 16:47:52 +0330

    added some codes

--- game.py
+++ game.py
@@ -6,16 +6,24 @@ import random
   #VARIABLES
 display_width = 576
 display_height = 1024
+background_image = pygame.image.load("assets/img/bg2.png")
+
 # START PYGAME MODULES
 pygame.init()
-main_screen = pygame.display.set_mode ((576, 1024))
+
+#Main Display In Game
+main_screen = pygame.display.set_mode ((display_width, display_height))
+
+#Game Timer
 clock = pygame.time.Clock
 while True:
-     for event in pygame.event.get()
+    for event in pygame.event.get()
        if event.type == pygame.Quit:
+        #END PYGAME MODULES 
           pygame.quit()
+          #TERMINATE PROGRAM
           sys.exit()
-
-      pygame.display.update()
+      main_screen.blit(background_image, (0, 0))    
+      pygame.display.update()  #SET GAME SPEED
       clock.tick(90)
 
